f7302704246b07c4515de986a4f04ea22301f08a
Pritam Kalbhor <physics.pritam@gmail.com>
2022-03-30 11:30:38 +0200

    New WFs added, renamed

--- python/alcaval_standard.py
+++ python/alcaval_standard.py
@@ -2,5 +2,24 @@
 from python.alcaval_steps import *
 workflows = dict()
 
-workflows[1] = ['MWGR_2022', ['MinimumBias2021', 'HLTDR2_2017', 'RECODR2_2017', 'HARVEST2017ZB']]
-workflows[2] = ['MWGR_2022', ['MinimumBias2021', 'RECODR2_2017', 'HARVEST2017ZB']]
+# CRUZET 2021
+workflows[1.1] = ['CRUZET2022', ['Cosmics2021', 'HLT_CRUZET2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+workflows[1.2] = ['CRUZET2022', ['Cosmics2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+workflows[1.3] = ['CRUZET2022', ['ExpressCosmics2021', 'HLT_CRUZET2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+workflows[1.4] = ['CRUZET2022', ['ExpressCosmics2021', 'RECO_CRUZET2021', 'HARVEST_CRUZET2021']]
+
+# PBT 2021
+workflows[2.1] = ['PBT2021', ['MinimumBias2021', 'HLT_PBT2021', 'RECO_PBT2021', 'HARVESTD']]
+workflows[2.2] = ['PBT2021', ['MinimumBias2021', 'RECOPE_PBT21', 'HARVESTD']]
+workflows[2.3] = ['PBT2021', ['ZeroBias2021', 'HLT_PBT2021', 'RECO_PBT2021', 'HARVESTD']]
+workflows[2.4] = ['PBT2021', ['ZeroBias2021', 'RECOPE_PBT21', 'HARVESTD']]
+workflows[2.5] = ['PBT2021', ['HLTPhysics2021', 'HLT_PBT2021', 'RECO_PBT2021', 'HARVESTD']]
+workflows[2.6] = ['PBT2021', ['HLTPhysics2021', 'RECOPE_PBT21', 'HARVESTD']]
+
+# MWGR 2022
+workflows[3.1] = ['MWGR2022', ['HcalNZS2022', 'HLT_MWGR2022', 'RECO_MWGR2022', 'HARVEST_MWGR2022']]
+workflows[3.2] = ['MWGR2022', ['HcalNZS2022', 'RECO_MWGR2022', 'HARVEST_MWGR2022']]
+
+# CRAFT 2022
+workflows[4.1] = ['CRAFT2022', ['HLTPhysics2022', 'HLT_CRAFT22', 'RECO_CRAFT2022', 'HARVEST_CRAFT22']]
+workflows[4.2] = ['CRAFT2022', ['HLTPhysics2022', 'RECO_CRAFT2022', 'HARVEST_CRAFT22']]
--- python/alcaval_steps.py
+++ python/alcaval_steps.py
@@ -2,39 +2,95 @@ from __future__ import absolute_import
 from .core import *
 
 steps = Steps()
+
+## Step1
+# CRUZET 2021
+steps['ExpressCosmics2021']={'INPUT':InputInfo(dataSet='/ExpressCosmics/Commissioning2021-Express-v1/FEVT',ls={345755: [[1, 990]]})}
+steps['Cosmics2021']={'INPUT':InputInfo(dataSet='/Cosmics/Commissioning2021-v1/RAW',ls={344518: [[1, 1892]]})}
+
+# PBT 2021
 steps['MinimumBias2021']={'INPUT':InputInfo(dataSet='/MinimumBias/Commissioning2021-v1/RAW',ls={346512: [[1,400]]})}
 steps['ZeroBias2021']={'INPUT':InputInfo(dataSet='/ZeroBias/Commissioning2021-v1/RAW', ls={346512: [[1,400]]})}
 steps['HLTPhysics2021']={'INPUT':InputInfo(dataSet='/HLTPhysics/Commissioning2021-v1/RAW',ls={346512: [[1,400]]})}
 
-# step1 HLT: for run3
+# MWGR 2022
+steps['HcalNZS2022']={'INPUT':InputInfo(dataSet='/HcalNZS/Commissioning2022-v1/RAW',ls={347807: [[1, 838]]})}
+
+# CRAFT 2022
+steps['HLTPhysics2022']={'INPUT':InputInfo(dataSet='/HLTPhysics/Commissioning2022-v1/RAW',ls={348338: [[1,180]]})}
+
+
+# Step2 HLT: for run3
 step2Defaults = {'--process':'reHLT',
                       '-s':'L1REPACK,HLT',
                       '--conditions':'auto:run3_data',
                       '--data':'',
                       '--eventcontent': 'FEVTDEBUGHLT',
                       '--datatier': 'FEVTDEBUGHLT',
+                      '--era' : 'Run3',
+                      '--scenario' : 'pp'
                       }
 
-hltKeyDefault='GRun'
-steps['HLTDR2_2017'] = merge( [ {'-s':'L1REPACK,HLT:%s'%hltKeyDefault,},{'--era' : 'Run3'}, step2Defaults ] )
+steps['HLT_PBT2021'] = merge( [ {'-s':'L1REPACK,HLT:GRun',},
+                                {'--procModifiers'  : 'siPixelQualityRawToDigi'},
+                                {'--customise'      : 'Configuration/DataProcessing/RecoTLR.customisePostEra_Run3,RecoLocalCalo/Configuration/customiseHBHEreco.hbheUseM0FullRangePhase1'},
+                                step2Defaults ] )
 
-# step3
+# Cosmics
+steps['HLT_CRUZET2021'] = merge( [ {'--scenario': 'cosmics', '--datatier': 'FEVTDEBUG', 
+                                    '--eventcontent': 'FEVTDEBUG', '--magField': '0T'}, step2Defaults] )
+steps['HLT_MWGR2022'] = merge( [ steps['HLT_CRUZET2021'] ] )
+steps['HLT_CRAFT22'] = merge( [ {'--scenario': 'cosmics', '--datatier': 'FEVTDEBUG', '--eventcontent': 'FEVTDEBUG'}, step2Defaults] )
+
+
+# Step3 RECO: for run3
 step3Defaults = {
-                  '-s'            : 'RAW2DIGI,L1Reco,RECO,EI,PAT,DQM:DQMOffline+offlineValidationHLTSource',
+                  '-s'            : 'RAW2DIGI,L1Reco,RECO,EI,PAT,DQM',
                   '--conditions'  : 'auto:run3_data',
                   '--no_exec'     : '',
                   '--data'        : '',
                   '--datatier'    : 'RECO,DQMIO',
                   '--eventcontent': 'RECO,DQM',
+                  '--era'         : 'Run3',
+                  '--process'     : 'reRECO',
+                  '--scenario'    : 'pp'
                   }
 
-steps['RECODR2_2017']=merge([{'--scenario':'pp','--era':'Run3','--customise':'Configuration/DataProcessing/RecoTLR.customisePostEra_Run3,RecoLocalCalo/Configuration/customiseHBHEreco.hbheUseM0FullRangePhase1'},step3Defaults])
+steps['RECO_PBT2021']=merge([
+                            {'-s' : 'RAW2DIGI,L1Reco,RECO,EI,PAT,DQM:DQMOffline+offlineValidationHLTSource'},
+                            {'--procModifiers'  : 'siPixelQualityRawToDigi'},
+                            {'--customise':'Configuration/DataProcessing/RecoTLR.customisePostEra_Run3,RecoLocalCalo/Configuration/customiseHBHEreco.hbheUseM0FullRangePhase1'},
+                            step3Defaults])
 
-#data
-steps['HARVESTD']={'-s':'HARVESTING',
+steps['RECOPE_PBT21']=merge([
+                            {'--procModifiers'  : 'siPixelQualityRawToDigi'},
+                            {'--customise':'Configuration/DataProcessing/RecoTLR.customisePostEra_Run3,RecoLocalCalo/Configuration/customiseHBHEreco.hbheUseM0FullRangePhase1'},
+                            step3Defaults])
+
+# Cosmics
+steps['RECO_CRUZET2021']=merge([{'--scenario':'cosmics',
+                                '-s'         : 'RAW2DIGI,L1Reco,RECO,DQM',
+                                '--magField' : '0T',
+                                },
+                                step3Defaults]
+                              )
+steps['RECO_MWGR2022']=merge([ steps['RECO_CRUZET2021'] ])
+steps['RECO_CRAFT2022']=merge([{'--scenario':'cosmics', '-s' : 'RAW2DIGI,L1Reco,RECO,DQM'},step3Defaults])
+
+
+# Step4 HARVESTING
+steps['HARVESTD']={'-s':'HARVESTING:dqmHarvesting',
                    '--conditions':'auto:run3_data',
-                   '--data':'',
-                   '--filetype':'DQM',
-                   '--scenario':'pp'}
+                   '--data'     :'',
+                   '--no_exec'  :'',
+                   '--filetype' :'DQM',
+                   '--scenario' :'pp',
+                   '--era'      :'Run3'
+                   }
 
-steps['HARVEST2017ZB'] = merge([ {'--era':'Run3','-s':' HARVESTING:@rerecoZeroBiasFakeHLT+@miniAODDQM'}, steps['HARVESTD'] ])
+steps['HARVEST_PBT2021'] = merge([ steps['HARVESTD'] ])
+
+# Cosmics
+steps['HARVEST_CRUZET2021'] = merge([ {'--scenario':'cosmics'}, steps['HARVESTD'] ])
+steps['HARVEST_MWGR2022'] = merge([ {'--scenario':'cosmics'}, steps['HARVESTD'] ])
+steps['HARVEST_CRAFT22'] = merge([ {'--scenario':'cosmics'}, steps['HARVESTD'] ])
